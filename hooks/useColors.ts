5ce08eb274c593ea3b9fb9447f981385d17b46cf
ofershaham2-cmyk <ofer.shaham2@gmail.com>
2026-06-07 22:49:17 +0000

    Revert "Update project to use Expo React Native for web and mobile applications"
    This reverts commit 8442d6bcd4ad537422e3b920472281ab7c80648c.

--- hooks/useColors.ts
+++
@@ -1,7 +0,0 @@
-import { useColorScheme } from 'react-native';
-import { lightColors, darkColors, type Colors } from '@/constants/colors';
-
-export function useColors(): Colors {
-  const scheme = useColorScheme();
-  return scheme === 'dark' ? darkColors : lightColors;
-}
